2cdac27ba25e3e6ab0078ed4041c86f81f2fccf4
stiven212 <stivfunk2000@hotmail.com>
2021-05-19 23:36:34 -0500

    Subida de archivos

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:project/src/pages/alert_page.dart';
+import 'package:project/src/providers/menu_provider.dart';
+import 'package:project/src/utils/icono_string_util.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes'),
+      ),
+      body: _lista(),
+    );
+  }
+}
+
+Widget _lista() {
+  /* menuProvider.cargarData().then((opciones) {
+    print('_lista');
+    print(opciones);
+  }); */
+  return FutureBuilder(
+    future: menuProvider.cargarData(),
+    initialData: [],
+    builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+      return ListView(
+        children: _listaItems(snapshot.data, context),
+      );
+    },
+  );
+}
+
+_listaItems(List<dynamic> data, BuildContext context) {
+  final List<Widget> opciones = [];
+  data.forEach((opt) {
+    final widgetTemp = ListTile(
+      title: Text(opt['texto']),
+      leading: getIcon(opt['icon']),
+      trailing: Icon(Icons.keyboard_arrow_down_outlined, color: Colors.blue),
+      onTap: () {
+        final route = MaterialPageRoute(builder: (context) => AlertPage());
+        Navigator.push(context, route);
+      },
+    );
+
+    opciones..add(widgetTemp)..add(Divider());
+  });
+  return opciones;
+}
---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePageTemp extends StatelessWidget {
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(children: _crearItemsCorta()),
+    );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> lista = <Widget>[];
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista..add(tempWidget)..add(Divider());
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map((item) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text(item + '!'),
+            subtitle: Text('Cualquier cosa'),
+            leading: Icon(Icons.access_alarms),
+            trailing: Icon(Icons.arrow_right_sharp),
+            onTap: () {},
+          )
+        ],
+      );
+    }).toList();
+  }
+}
