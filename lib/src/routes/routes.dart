5a5f50d450e7e7d72be92d0b88817beb4aba64e3
stiven212 <stivfunk2000@hotmail.com>
2021-05-22 15:52:10 -0500

    project finished

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -5,6 +5,7 @@ import 'package:project/src/pages/avatar_page.dart';
 import 'package:project/src/pages/card_page.dart';
 import 'package:project/src/pages/home_page.dart';
 import 'package:project/src/pages/input_page.dart';
+import 'package:project/src/pages/listview_page.dart';
 import 'package:project/src/pages/slider_page.dart';
 
 Map<String, WidgetBuilder> getApplicationRoutes() {
@@ -16,5 +17,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
     'slider': (BuildContext context) => SliderPage(),
+    'list': (BuildContext context) => ListaPage(),
   };
 }
